83ed1f466b8391862832b4b8f4aad20ac5824a0b
William Kennedy <alexkenned@gmail.com>
2020-03-02 21:50:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,29 +4,21 @@ require 'pry'
 def load_library(fp)
   emoticons = YAML.load_file(fp)
   
-  # i = 0 
   hash = {
     "get_meaning" => {},
     "get_emoticon"=> {}
   }
-  # je = {}
   emoticons.each do |key, value|
     
       hash["get_emoticon"][value[0]] = emoticons[key][1]
       hash["get_meaning"][value[1]] = key
     end
-
-  # while i < emoticons.length do
-  #   je[emoticons[i][1]] = emoticons[i][0]
-  #   i+=1 
-  # end
-  
-  #binding.pry
   return hash
 end
 
-def get_japanese_emoticon
-
+def get_japanese_emoticon(fp, emoticon)
+    loaded = load_library(fp)
+    binding.pry 
 end
 
 def get_english_meaning
