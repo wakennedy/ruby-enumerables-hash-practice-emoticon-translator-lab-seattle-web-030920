c3fa0c68fbf169ff44d990673022e6e2733c406c
William Kennedy <alexkenned@gmail.com>
2020-03-02 21:04:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,31 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(fp)
+  emoticons = YAML.load_file(fp)
+  
+  
+  i = 0 
+  je = {}
+  while i < emoticons.length do 
+    je[emoticons[i][1]] = emoticons[i][1]
+    i+=1 
+  end
+  
+  hash = {
+    :get_meaning => {
+      
+    },
+    :get_emoticon=> {
+      
+    }
+  }
+  return hash
+  binding.pry 
 end
 
 def get_japanese_emoticon
-  # code goes here
+
 end
 
 def get_english_meaning
